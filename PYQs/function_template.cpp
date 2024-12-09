40674ef1a3714dca77d9182b10fc9d41a99aa596
ashboi005 <125044617+ashboi005@users.noreply.github.com>
2024-12-09 23:47:22 +0530

    fixed function_template

--- PYQs/function_template.cpp
+++ PYQs/function_template.cpp
@@ -1,7 +1,7 @@
 #include <iostream.h>
 #include <conio.h>
 
-template <class T>
+template <class T> //wrote class here because in Turbo CPP even function template needs to be made like this using the keyword class instead of typename
 T add(T a, T b) {
     return a + b;
 }
